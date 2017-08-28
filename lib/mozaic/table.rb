783db94c394f1f87086fc1ec72bce6eba7c42707
mnc <nobuya.sasaki@speee.jp>
2017-08-28 15:48:48 +0900

    create table class to fetch records and columns

---
+++ lib/mozaic/table.rb
@@ -0,0 +1,29 @@
+require 'active_record'
+
+module Mozaic
+  class Table
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def records
+      @records ||= result.to_ary
+    end
+
+    def columns
+      @columns ||= result.columns
+    end
+
+    private
+
+    def result
+      @result ||= conn.select_all("SELECT * FROM #{@name};")
+    end
+
+    def conn
+      @conn ||= ActiveRecord::Base.connection
+    end
+  end
+end
