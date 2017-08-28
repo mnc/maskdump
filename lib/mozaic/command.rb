8eaf44e73102d82e75ba5bef30e6e7006121af71
mnc <nobuya.sasaki@speee.jp>
2017-08-28 15:47:08 +0900

    dump table definition

--- lib/mozaic/command.rb
+++ lib/mozaic/command.rb
@@ -12,6 +12,14 @@ module Mozaic
     desc "dump", "dump"
     def dump(yaml)
       @setting = Setting.new(yaml, options)
+      connect_db
+      ActiveRecord::Tasks::DatabaseTasks.structure_dump(@setting.db_setting_hash, options[:dest])
+    end
+
+    private
+
+    def connect_db
+      ActiveRecord::Base.establish_connection(@setting.db_setting_hash)
     end
   end
 end
