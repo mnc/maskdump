be1657d64956e16b6cbd9fb3a05a66d3bdc89130
mnc <nobuya.sasaki@speee.jp>
2017-08-28 15:55:55 +0900

    fetch records and generate insert statement masked

--- lib/mozaic.rb
+++ lib/mozaic.rb
@@ -3,4 +3,7 @@ require "mozaic/version"
 module Mozaic
   autoload :Command, "mozaic/command"
   autoload :Setting, 'mozaic/setting'
+  autoload :Table, 'mozaic/table'
+  autoload :Mask, 'mozaic/mask'
+  autoload :BulkInsert, 'mozaic/bulk_insert'
 end
--- lib/mozaic/command.rb
+++ lib/mozaic/command.rb
@@ -14,6 +14,17 @@ module Mozaic
       @setting = Setting.new(yaml, options)
       connect_db
       ActiveRecord::Tasks::DatabaseTasks.structure_dump(@setting.db_setting_hash, options[:dest])
+      @setting.table_settings.each do |table_setting|
+        table = Table.new(table_setting[:name])
+        next if table.records.blank?
+        masked_records = Mask.new(table.records, table_setting[:columns]).mask
+        query = BulkInsert.generate(table.name, masked_records, table.columns, @setting)
+        File.open(options[:dest], 'a') do |f|
+          f.puts "\n"
+          f.puts query
+          f.puts "\n"
+        end
+      end
     end
 
     private
